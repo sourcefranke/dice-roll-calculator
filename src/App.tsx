f6a3debd0da2726dd140bbea01073bc716b5ad92
Lukas Tietenberg <lukas.tietenberg@outlook.de>
2024-09-09 17:15:15 +0200

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,12 @@ function App() {
 
     return (
         <div className="App">
-            <header className="App-header">
-                <h1>Welcome to Dice Roll Calculator!</h1>
-                <p>This is a web app for calculating dice rolls.<br/>You can define your throw via a formula and roll for results.</p>
-                <button onclick='' style={{
-                    cursor: 'pointer',
-                    fontSize: 'xxx-large'
-                }}
-                >
-                    Start
-                </button>
-                <p>Source code hosted on <a href="https://github.com/sourcefranke/dice-roll-calculator">GitHub</a></p>
-            </header>
+            <h1>Welcome to Dice Roll Calculator!</h1>
+            <p>This is a web app for calculating dice rolls.<br/>You can define your throw via a formula and roll for results.</p>
+            <button style={{cursor: 'pointer', fontSize: 'xx-large'}}>
+                Test
+            </button>
+            <p>Source code hosted on <a href="https://github.com/sourcefranke/dice-roll-calculator">GitHub</a></p>
         </div>
     );
 }
